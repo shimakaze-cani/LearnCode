7340def8cbf20f0f61ce0495df06d385bcb521d7
cani <shimakaze_cani@outlook.com>
2022-08-14 21:52:55 +0800

    220814 create C_4.md from lap

--- C_test/C_test/HelloWorld.c
+++ C_test/C_test/HelloWorld.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
 
+int src[10] = { 1,2,3,4,5,6,7,8,9 };
+int dst[10] = { 0 };
+
+int data_copy_c(void) {
+
+	for (int i = 0; i < 10; i++)
+		dst[i] = src[i];
+	return 0;
+}
+
+int  data_copy_asm(void) {
+	
+	__asm
+	{
+		LDR R0, =src
+		LDR R1, =dst
+		MOV R2, #10
+	LOOP:
+		LDR R3, [R0], #4
+		STR R3, [R1], #4
+		SUBS R2, R2, #1
+		BNE LOOP
+	}
+}
 void main() {
 	
 	
@@ -20,4 +44,9 @@ void main() {
 	
 
 	printf("Hello World!");
+
+	
+	
+	
 } 
+
